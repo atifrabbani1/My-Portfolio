1533bb8f0788f07e17acc220ea7a8d672236f016
Atif Rabbani <atifrabbani1@yahoo.com>
2019-09-15 12:04:26 -0400

    Developed Portfolio Page and made some major changes

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -3,16 +3,6 @@ $(document).ready(function () {
     $("#resume").hide();
     $("#portfolio").hide();
     $("#contact").hide();
-    $('.images').slick({
-        initialSlide:1,
-        infinite: true,
-        speed: 2000,
-        slidesToShow: 2,
-        slidesToScroll: 2,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        
-    });
     $('.modal').modal();
 });
 $("#profile-btn").on("click", function () {
@@ -28,6 +18,7 @@ $("#resume-btn").on("click", function () {
     $("#portfolio").hide();
     $("#contact").hide();
     $("#resume").fadeIn(3000);
+    
 })
 $("#portfolio-btn").on("click", function () {
     event.preventDefault();
@@ -43,4 +34,45 @@ $("#contact-btn").on("click", function () {
     $("#portfolio").hide();
     $("#contact").fadeIn(3000);
 })
-$(".tabs").tabs('swipeable', true);
+//$(".tabs").tabs('swipeable', true);
+
+var slideIndex = 0;
+showSlides1();
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+function showSlides1() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides1, 2000); // Change image every 2 seconds
+}
